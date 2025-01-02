9aff3291166a7a3928a7d9d5e5445d6e4b891060
Paul Chasseuil <you@example.com>
2025-01-02 14:54:41 +0100

    Initial setup of PHPUnit and basic test cases

---
+++ tests/MatchsDeLaNuitTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class MatchsDeLaNuitTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
